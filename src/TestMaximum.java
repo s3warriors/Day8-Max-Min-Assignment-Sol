08fa321556db61c098af0bb0e43f69178956ab1e
s3warriors <satveerswami982@gmail.com>
2024-12-13 12:54:17 +0530

    Added: Print Maximum to Standard Output

--- src/TestMaximum.java
+++ src/TestMaximum.java
@@ -2,15 +2,22 @@ import java.util.Arrays;
 
 public class TestMaximum<T extends Comparable<T>> {
     public static <T extends Comparable<T>> T findMax(T... elements) {
-        return Arrays.stream(elements).max(T::compareTo).orElse(null);
+        T max = Arrays.stream(elements).max(T::compareTo).orElse(null);
+        printMax(max);
+        return max;
+    }
+
+    public static <T> void printMax(T max) {
+        System.out.println("The maximum value is: " + max);
     }
 
     public static void main(String[] args) {
-        System.out.println("Max of (10, 5, 8, 15): " + findMax(10, 5, 8, 15));
-        System.out.println("Max of (3.5, 1.2, 4.8, 2.1): " + findMax(3.5f, 1.2f, 4.8f, 2.1f));
-        System.out.println("Max of (Apple, Peach, Banana, Grape): " + findMax("Apple", "Peach", "Banana", "Grape"));
+        findMax(10, 5, 8, 15);
+        findMax(3.5f, 1.2f, 4.8f, 2.1f);
+        findMax("Apple", "Peach", "Banana", "Grape");
     }
 }
+
 
 
 
