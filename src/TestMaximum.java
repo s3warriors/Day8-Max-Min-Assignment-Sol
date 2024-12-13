07b577adc06ea1e8fb0cf14f0d486a4d2240351b
s3warriors <satveerswami982@gmail.com>
2024-12-13 12:52:37 +0530

    Added: Extend Method to More Than Three Parameters

--- src/TestMaximum.java
+++ src/TestMaximum.java
@@ -1,19 +1,16 @@
-public class TestMaximum {
-    // Method to find maximum of three strings
-    public static String findMax(String a, String b, String c) {
-        String max = a;
-        if (b.compareTo(max) > 0) {
-            max = b;
-        }
-        if (c.compareTo(max) > 0) {
-            max = c;
-        }
-        return max;
+import java.util.Arrays;
+
+public class TestMaximum<T extends Comparable<T>> {
+    public static <T extends Comparable<T>> T findMax(T... elements) {
+        return Arrays.stream(elements).max(T::compareTo).orElse(null);
     }
 
     public static void main(String[] args) {
-        System.out.println("Max of (Apple, Peach, Banana): " + findMax("Apple", "Peach", "Banana")); // Test Case
+        System.out.println("Max of (10, 5, 8, 15): " + findMax(10, 5, 8, 15));
+        System.out.println("Max of (3.5, 1.2, 4.8, 2.1): " + findMax(3.5f, 1.2f, 4.8f, 2.1f));
+        System.out.println("Max of (Apple, Peach, Banana, Grape): " + findMax("Apple", "Peach", "Banana", "Grape"));
     }
 }
+
 
 
