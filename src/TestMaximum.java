23aff098b4a50d392da132e201910f816758e0d2
s3warriors <satveerswami982@gmail.com>
2024-12-13 12:51:00 +0530

    Added: Find Maximum of Three Strings

--- src/TestMaximum.java
+++ src/TestMaximum.java
@@ -1,7 +1,7 @@
 public class TestMaximum {
-    // Method to find maximum of three floats
-    public static Float findMax(Float a, Float b, Float c) {
-        Float max = a;
+    // Method to find maximum of three strings
+    public static String findMax(String a, String b, String c) {
+        String max = a;
         if (b.compareTo(max) > 0) {
             max = b;
         }
@@ -12,9 +12,8 @@ public class TestMaximum {
     }
 
     public static void main(String[] args) {
-        System.out.println("Max of (3.5, 1.2, 4.8): " + findMax(3.5f, 1.2f, 4.8f)); // Test Case
-        System.out.println("Max of (1.2, 5.8, 4.8): " + findMax(1.2f, 5.8f, 4.8f)); // Test Case
-        System.out.println("Max of (1.2, 3.5, 7.9): " + findMax(1.2f, 3.5f, 7.9f)); // Test Case
+        System.out.println("Max of (Apple, Peach, Banana): " + findMax("Apple", "Peach", "Banana")); // Test Case
     }
 }
+
 
